66c44089dfba4dd6a66fc6c34cd224fb47f3df22
Kgadrw <kalisagad05@gmail.com>
2025-06-13 13:51:04 +0200

    last push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
 import Experience from "../components/Experience"
-import Skills from "../components/Skills"
 import Projects from "../components/Projects"
 import Contact from "../components/Contact"
 import Footer from "../components/Footer"
